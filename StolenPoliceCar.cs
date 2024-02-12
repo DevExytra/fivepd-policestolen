514f27f49fbcf0b98a2d387051b7d86f2b841156
Maurice <exytra@protonmail.com>
2024-02-12 16:54:21 +0100

    Changed the entire base of the code, I also changed the callout.

--- StolenPoliceCar.cs
+++ StolenPoliceCar.cs
@@ -8,19 +8,18 @@ using FivePD.API;
 
 namespace StolenPoliceVehicle
 {
-    [CalloutProperties("Stolen Police Car", "ERLS Team", "1.2")]
+    [CalloutProperties("Stolen Emergency Vehicle", "ERLS Team", "1.3")]
     public class StolenPoliceCar : Callout
     {
-        private Vehicle car;
-        private Ped suspect;
-        private Ped suspect2;
-
-        private readonly Random rnd = new Random();
+        private Vehicle _car;
+        private Ped _driver;
+        private Ped _shooter;
+        private readonly Random _rnd = new Random();
 
         public StolenPoliceCar()
         {
             Random random = new Random();
-            float offsetX = random.Next(100, 700); 
+            float offsetX = random.Next(100, 700);
             float offsetY = random.Next(100, 700);
             InitInfo(World.GetNextPositionOnStreet(Game.PlayerPed.GetOffsetPosition(new Vector3(offsetX, offsetY, 0))));
             ShortName = "Stolen Police Car";
@@ -29,123 +28,175 @@ namespace StolenPoliceVehicle
             StartDistance = 150f;
         }
 
+        // Gets a random weapon out of this list
+        private WeaponHash GetRandomWeapon()
+        {
+            List<WeaponHash> weapons = new List<WeaponHash>
+            {
+                WeaponHash.APPistol,
+                WeaponHash.Pistol,
+                WeaponHash.Pistol50,
+                WeaponHash.Revolver
+            };
+            return weapons[_rnd.Next(weapons.Count)];
+        }
+
+        private VehicleHash GetRandomEmergencyVehicleHash()
+        {
+            List<VehicleHash> vehicle = new List<VehicleHash>
+            {
+                VehicleHash.Police,
+                VehicleHash.Police2,
+                VehicleHash.Police3,
+                VehicleHash.Police4,
+                VehicleHash.Policeb,
+                VehicleHash.PoliceOld1,
+                VehicleHash.FireTruk,
+                VehicleHash.Ambulance
+            };
+            return vehicle[_rnd.Next(vehicle.Count)];
+        }
+
+        // Gets a random driver out of this list
+        private PedHash GetRandomDriver()
+        {
+            List<PedHash> ped = new List<PedHash>
+            {
+                PedHash.Abigail,
+                PedHash.Clay,
+                PedHash.Andreas,
+                PedHash.Bankman01,
+                PedHash.Brad,
+                PedHash.Hao,
+                PedHash.Barry,
+                PedHash.Nigel
+            };
+            return ped[_rnd.Next(ped.Count)];
+        }
+
+        // Gets a random shooter out of this list
+        private PedHash GetRandomShooter()
+        {
+            List<PedHash> ped = new List<PedHash>
+            {
+                PedHash.AfriAmer01AMM,
+                PedHash.Cletus,
+                PedHash.Dale,
+                PedHash.Franklin,
+                PedHash.Floyd,
+                PedHash.DoaMan, 
+                PedHash.TaoCheng,
+                PedHash.Trevor
+            };
+            return ped[_rnd.Next(ped.Count)];
+        }
+
+
         public async override void OnStart(Ped player)
         {
-            suspect = await World.CreatePed(RandomUtils.GetRandomPed(), Location + 1);
-            suspect.AlwaysKeepTask = true;
-            suspect.BlockPermanentEvents = true;
-            
-            if (SpawnChance())
+            double chance = 0.75;
+            bool spawn = SpawnChance(chance);
+
+            if (spawn)
             {
-                suspect2 = await World.CreatePed(RandomUtils.GetRandomPed(), Location + 1);
-                suspect2.AlwaysKeepTask = true;
-                suspect2.BlockPermanentEvents = true;
+                // Creates The Driver and seats him into the vehicle
+                _driver = await World.CreatePed(GetRandomDriver(), Location + 1);
+                _driver.AlwaysKeepTask = true;
+                _driver.BlockPermanentEvents = true;
                 
-                // Spawns the car with bot suspects
-                car = await World.CreateVehicle(VehicleHash.Police, Location);
-                suspect.SetIntoVehicle(car, VehicleSeat.Driver);
-                suspect2.SetIntoVehicle(car, VehicleSeat.Passenger);
+                // Creates the Shooter
+                _shooter = await World.CreatePed(GetRandomShooter(), Location + 1);
+                _shooter.Weapons.Give(GetRandomWeapon(), 100, true, true);
+                _shooter.Accuracy = 98;
+                _shooter.AlwaysKeepTask = true;
+                _shooter.BlockPermanentEvents = true;
                 
-                // Gives suspect 2 the weapon
-                suspect2.Weapons.Give(getRandomWeapon(), 100, true, true);
-                suspect2.Task.VehicleShootAtPed(player);
-                // Makes the Driver flee from the player
-                suspect.Task.FleeFrom(player);
-                car.IsSirenActive = true;
+                // Creates the Emergency Vehicle and seats both Suspects in!
+                _car = await World.CreateVehicle(GetRandomEmergencyVehicleHash(), Location);
+                _driver.SetIntoVehicle(_car, VehicleSeat.Driver);
+                _shooter.SetIntoVehicle(_car, VehicleSeat.Passenger);
+                _car.IsSirenActive = true;
                 
-                // Registers the suspect if spawned
-                Pursuit.RegisterPursuit(suspect2);
+                // Tasks Both Suspects do to something
+                API.SetDriveTaskDrivingStyle(_driver.GetHashCode(), 524852);
+                _driver.Task.FleeFrom(player);
+                _shooter.Task.VehicleShootAtPed(player);
+                
+                // Attaches the blip to the vehicle
+                _car.AttachBlip();
             }
             else
             {
                 
-                car = await World.CreateVehicle(VehicleHash.Police, Location);
-                suspect.SetIntoVehicle(car, VehicleSeat.Driver);
+                // Creates the Driver
+                _driver = await World.CreatePed(GetRandomDriver(), Location + 1);
+                _driver.AlwaysKeepTask = true;
+                _driver.BlockPermanentEvents = true;
                 
-                // Sets the suspect to flee 
-                API.SetDriveTaskDrivingStyle(suspect.GetHashCode(), 524852);
-                car.IsSirenActive = true;
-                suspect.Task.FleeFrom(player);
-
-                // Registers suspect 1
-                Pursuit.RegisterPursuit(suspect);
+                // Creates the Emergency Vehicle and seats the Suspects in!
+                _car = await World.CreateVehicle(GetRandomEmergencyVehicleHash(), Location);
+                _driver.SetIntoVehicle(_car, VehicleSeat.Driver);
+                _car.IsSirenActive = true;
+                
+                // Tasks the Suspect to flee
+                API.SetDriveTaskDrivingStyle(_driver.GetHashCode(), 524852);
+                _driver.Task.FleeFrom(player);
+                
+                // Attaches the Blip to the vehicle
+                _car.AttachBlip();
             }
-
-            // Attach blips to suspects and the police car
-            suspect.AttachBlip();
-            car.AttachBlip();
-        }
-
-        // Grandpa Rex gave me this <3
-        public override void OnCancelAfter()
-        {
-            base.OnCancelAfter();
-            
-            try
-            {
-                if (!suspect.IsAlive || suspect.IsCuffed) return;
-                suspect.Task.WanderAround(); suspect.AlwaysKeepTask = false; suspect.BlockPermanentEvents = false;
-            }
-            catch { EndCallout(); }
-
-            try
-            {
-                if (!suspect2.IsAlive || suspect2.IsCuffed) return;
-                suspect2.Task.WanderAround(); suspect2.AlwaysKeepTask = false; suspect2.BlockPermanentEvents = false;
-            }
-            catch { EndCallout(); }
-            
-        }
-
-        // Grandpa Rex gave me this <3
-        public override void OnCancelBefore()
-        {
-            base.OnCancelBefore();
-
-            try
-            {
-                if (!suspect.IsAlive || suspect.IsCuffed) return;
-                suspect.Task.WanderAround(); suspect.AlwaysKeepTask = false; suspect.BlockPermanentEvents = false;
-            }
-            catch { EndCallout(); }
-
-            try
-            {
-                if (!suspect2.IsAlive || suspect2.IsCuffed) return;
-                suspect2.Task.WanderAround(); suspect2.AlwaysKeepTask = false; suspect2.BlockPermanentEvents = false;
-            }
-            catch { EndCallout(); }
         }
         
-        // Nice Hashing
-        private WeaponHash getRandomWeapon()
-        {
-            List<WeaponHash> weapons = new List<WeaponHash>
-            {
-                WeaponHash.APPistol
-            };
-            return weapons[rnd.Next(weapons.Count)];
-        }
-
         public async override Task OnAccept()
         {
             InitBlip();
             UpdateData();
         }
         
-        private bool SpawnChance()
+        // Thanks to Grandpa Rex!
+        public override void OnCancelAfter()
         {
-            // This is the randomizing amount
-            double spawnChance = 0.3;
+            base.OnCancelAfter();
+            try
+            {
+                // Clears the driver
+                if (!_driver.IsAlive && !_shooter.IsAlive || _driver.IsCuffed && _shooter.IsCuffed) return;
+                _driver.Task.WanderAround();
+                _driver.AlwaysKeepTask = false;
+                _driver.BlockPermanentEvents = false;
+                
+                // Clears the Shooter
+                _shooter.Task.WanderAround();
+                _shooter.AlwaysKeepTask = false;
+                _shooter.BlockPermanentEvents = false;
+            }
+            catch
+            {
+                EndCallout();
+            }
+        }
 
-            // Initialize random number generator
+        // Thanks to Grandpa Rex!
+        public override void OnCancelBefore()
+        {
+            // Clears the Driver
+            base.OnCancelBefore();
+            if (!_driver.IsAlive && !_shooter.IsAlive || _driver.IsCuffed && _shooter.IsCuffed) return;
+            _driver.Task.WanderAround();
+            _driver.AlwaysKeepTask = false;
+            _driver.BlockPermanentEvents = false;
+            
+            // Clears the Shooter
+            _shooter.Task.WanderAround();
+            _shooter.AlwaysKeepTask = false;
+            _shooter.BlockPermanentEvents = false;
+        }        
+
+        static bool SpawnChance(double chance)
+        {
             Random random = new Random();
-
-            // Generate random number between 0 and 1
-            double randomValue = random.NextDouble();
-
-            // Check if random number is less than or equal to spawn chance
-            return randomValue < spawnChance;
-         }
-     }
+            double randomNumber = random.NextDouble();
+            return randomNumber < chance;
+        }
+    }
 }
